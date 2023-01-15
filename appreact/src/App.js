be1baf5623471364fbb2574501d2968769972d3e
Gustavo Castro <gustavocastrocs@gmail.com>
2023-01-14 22:09:16 -0300

    02.10 - Eventos

---
+++ appreact/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const App = () => {
+  function handleClick(event){
+    console.log(event.target)
+  }
+
+  function handleScroll(event){
+    console.log(event)
+  }
+  window.addEventListener('scroll', handleScroll);
+
+  return (
+    <div style={{height: '200vh'}}>
+      <button onClick={(event) => alert(event.target.innetText)}>Compre Violão.</button>
+    </div>
+    
+  )
+};
+
+export default App
